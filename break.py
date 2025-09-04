af86701b0e117ec7123d4c73f697fe78ed7f0f45
tunnu-sahni <tsahni313@gmail.com>
2025-09-04 23:31:41 +0530

    added break.py

---
+++ break.py
@@ -0,0 +1,56 @@
+# i = 1
+# while i<=2:
+#     print(i)
+
+#     if(i ==3):
+#         break
+#     i +=1
+
+# print("end of loop")
+
+
+
+# i = 5
+# while i<3:
+#     print(4)
+#     if(i ==2):
+#        print("end")
+
+
+
+# i = 0
+# while i<=5:
+#     if(i ==2):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+
+
+# list = [1,2,3,4,5]
+# for el in list:
+#     print(el)
+
+
+
+# list = [2,3,4,5,6,7,8]
+# for el in list:
+#     print(el)
+
+
+
+# nums = [2,3,4,5,6,7,7,8]
+# for val in nums:
+#     print(val)
+
+
+# tup = (1,2,3,4,5,6,8,999)
+# for nums in tup:
+#     print(nums)
+
+
+
+str = "tunnu sahni"
+for char in str:
+    print(char)      
