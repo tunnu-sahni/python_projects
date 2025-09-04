12e9317b1ba093e7fe8fa493c6783186996c10d7
tunnu-sahni <tsahni313@gmail.com>
2025-09-04 23:28:16 +0530

    added tuple.py

---
+++ tuple.py
@@ -0,0 +1,29 @@
+# tup =(3,4,5,6,7,8)
+# print(tup[3])
+
+
+# tup = (3,4,5,2,4,5)
+# print(tup[2])
+
+
+# tup = (3,4,5,6)
+# print(tup[2])
+
+
+# tup = (24,5,6,6,7,78,8,)
+# print(tup.index(5))
+
+
+
+# tup = (4,5,5,666,7,7,8,8)
+# print(tup.index(4))
+
+
+
+tup = (3,5,6,6,7,8,8,)
+print(tup.count(4))
+
+
+
+tup = (3,4,5,5,6,76,7,8,98,9)
+print(tup.count(5))
