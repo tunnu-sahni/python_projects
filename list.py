90a8b572640c0214e18717983cbd3d7078ce1fc5
tunnu-sahni <tsahni313@gmail.com>
2025-09-04 23:24:51 +0530

    added list.py

---
+++ list.py
@@ -0,0 +1,35 @@
+# list =[1,3,4,5]
+# list.append(2)
+# print(list)
+
+
+# list = [34,5,66,77,1,7,8,6]
+# list.append(5)
+# print(list)
+
+
+
+# list = [2,3,4,5,6]
+# list.sort(reverse =True)
+# print(list)
+
+
+
+# list = [2,34,5,6]
+# list.insert(1,2)
+# print(list)
+
+
+# list = [2,3,4,56,76,7,8]
+# list.insert(2,4)
+# print(list)
+
+
+list = [2,3,4,55,66,77,88]
+list.remove(3)
+print(list)
+
+
+list = [23,4,5,66,77,5,5,4]
+list.remove(5)
+print(list)
