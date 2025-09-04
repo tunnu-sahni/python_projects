a4b1229fd1a74b51343b64c6afd38243c9fa40e3
tunnu-sahni <tsahni313@gmail.com>
2025-09-04 23:29:59 +0530

    added set.py

---
+++ set.py
@@ -0,0 +1,24 @@
+# collection = (2,3,4,5,6,6,6)
+# print(collection)
+# print(len(collection))
+
+
+
+# null_set = ()
+# collection = set()
+# print(type(collection))
+
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.remove(2)
+# #collection.clear()
+# collection.pop()
+# print(collection)
+
+
+
+set1 ={1,6,2,3,4,5}
+set2 ={2,3,4,5,6}
+print(set1.intersection(set2))
