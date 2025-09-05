a0ed3310ab47655b7eeac8164e5bd3e18795f1d5
tunnu-sahni <tsahni313@gmail.com>
2025-09-05 15:17:55 +0530

    added range.py

---
+++ range.py
@@ -0,0 +1,27 @@
+# for i in range(10):
+#     print(i)
+
+
+# for i in range(20):
+#     print(i)
+
+
+
+# for i in range(2,20):
+#     print(i)
+
+
+
+# for i in range(3,10,3):
+#     print(i)
+
+
+for i in range(2,10):
+    print(i)
+
+
+
+
+n = int(input("enter number:"))
+for i in range(1,11):
+    print(n*i)
