510b58870b6ed52ef295b77b301437303080ce04
tunnu-sahni <tsahni313@gmail.com>
2025-09-07 17:59:52 +0530

    added lambda.py

--- lambda.py
+++ lambda.py
@@ -10,15 +10,49 @@
 # print(x(3))
 
 
-x = lambda a , b : a * b
-print(x(5 , 6))
+# x = lambda a , b : a * b
+# print(x(5 , 6))
 
 
 
-x = lambda a , b, c : a + b + c
-print(x(4 , 6, 7)) 
+# x = lambda a , b, c : a + b + c
+# print(x(4 , 6, 7)) 
 
 
 
-x = lambda a , b ,c : a + b * c
-print(x(4 , 5 ,9))
+# x = lambda a , b ,c : a + b * c
+# print(x(4 , 5 ,9))
+
+
+
+
+
+def myfunc(n):
+    return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
+
+
+
+def myfunc(n):
+    return lambda a : a * n
+
+mytriple = myfunc(3)
+
+print(mytriple(11))
+
+
+
+
+
+def myfunc(n):
+    return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler =myfunc(3)
+
+print(mydoubler(11))
+print(mytriple(11))
+
