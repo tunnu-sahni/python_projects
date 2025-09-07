e4f52921c75168d5c4db098691034338d415a058
tunnu-sahni <tsahni313@gmail.com>
2025-09-07 16:59:36 +0530

    added function.py

---
+++ function.py
@@ -0,0 +1,78 @@
+# def my_function():
+#     print("hello from a function")
+
+# my_function()
+
+
+
+# def my_function():
+#     print("welcome tunnu sir")
+
+# my_function()
+
+
+
+
+# def my_function():
+#     print("how are you tunnu what are say")
+
+# my_function()
+
+
+# def my_function():
+#     print("tunnu from a mangalayaran univrsity")
+
+# my_function()
+
+
+# def my_function(fname):
+#     print(fname )
+
+
+# my_function("emil")
+# my_function("tobias")
+# my_function("linus")
+
+
+
+# def my_function(fname):
+#     print(fname)
+
+
+# my_function("tunnu")
+# my_function("sahni")
+# my_function("raipur")
+
+
+# def my_functon(fname,lname):
+#     print(fname + " " + lname)
+
+
+# my_functon("tunnu","sahni")   
+
+
+
+# def my_function(*kids):
+#     print("the youngest child is " + kids[0])
+#     print("the youngesr child is " + kids[1])
+#     print("the youngest child is " +kids[2])
+
+
+# my_function("emil","tobias","linus")                                         
+
+
+
+# def my_function(child3, child2, child1):
+#     print("the youngeat child is " + child1)
+#     print("the youngest child is " + child2)
+#     print("the youngest child is " + child3)
+
+# my_function(child1 = "tunnu" , child2 = "sahni", child3 = "rise")
+
+
+
+def my_function(**kids):
+    print("his first name is " + kids["fname"])
+
+
+my_function(fname = "tobias", lname = "refsnes") 
