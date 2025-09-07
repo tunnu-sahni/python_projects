7be8bf4dd7f7e656ee65d771d9868188ab2d4c1f
tunnu-sahni <tsahni313@gmail.com>
2025-09-07 17:05:49 +0530

    added combineposional.py

---
+++ combineposional.py
@@ -0,0 +1,10 @@
+def my_function(a , b, /, *, c, d):
+    print(a+b+c+d)
+
+my_function(5 , 6, c =7 , d = 9)
+
+
+def my_function(a , b, /, *, c, d):
+    print(a+b+c+d)
+
+my_function(4 ,6, c = 8, d = 90) 
