25b6e0edef4da66869f9d7f9528b61897876680a
tunnu-sahni <tsahni313@gmail.com>
2025-09-07 17:18:32 +0530

    added positional-onlyargment.py

---
+++ positional-onlyargment.py
@@ -0,0 +1,45 @@
+# def my_function(x, /):
+#     print(x)
+
+
+# my_function(6)
+
+
+# def my_function(x):
+#     print(x)
+
+
+# my_function(x = 3)
+
+
+# def my_function(x , /):
+#     print(x)
+
+
+# my_function(6)
+
+
+# def my_function(x):
+#     print(x)
+
+
+# my_function(x = 3)
+
+
+# def my_function(a):
+#     print(a)
+
+# my_function(a = 5)
+
+
+
+# def my_function(* , x):
+#     print(x)
+
+# my_function(x = 3)
+
+
+def my_function(x):
+    print(x)
+
+my_function(67)
