e53d1f4b0f8fce20cc7352d055b870946c031976
tunnu-sahni <tsahni313@gmail.com>
2025-09-07 17:37:18 +0530

    added lambda.py

---
+++ lambda.py
@@ -0,0 +1,24 @@
+# x = lambda a : a + 10
+# print(x(5))
+
+
+# x = lambda a : a - 10
+# print(x(5))
+
+
+# x = lambda a : a * 10
+# print(x(3))
+
+
+x = lambda a , b : a * b
+print(x(5 , 6))
+
+
+
+x = lambda a , b, c : a + b + c
+print(x(4 , 6, 7)) 
+
+
+
+x = lambda a , b ,c : a + b * c
+print(x(4 , 5 ,9))
