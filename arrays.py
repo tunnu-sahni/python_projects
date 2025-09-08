ccc7f39fa44ad5387278e87062525ff5f97e68ab
tunnu-sahni <tsahni313@gmail.com>
2025-09-08 22:38:26 +0530

    added arrays.py

---
+++ arrays.py
@@ -0,0 +1,28 @@
+# cars = ["ford" , "toyota", "bmw"]
+# print(cars)
+
+
+
+# cars[0] = "Toyota"
+
+# print(cars)
+
+
+# cars = ["tunnu", "sahni", "raipur"]
+
+# cars.pop(1)
+# print(2)
+
+
+# cars = ["toyota", "honda", "yamaha"]
+
+# cars.remove("honda")
+
+# print(cars)
+
+
+cars = ["toyota", "yamaha", "volvo"]
+
+cars.count("toyota")
+
+print(cars)
