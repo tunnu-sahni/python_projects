0ad57039d549b002fb7a86b77c6ee26f99e74321
tunnu-sahni <tsahni313@gmail.com>
2025-09-09 21:28:16 +0530

    added classesobject.py

---
+++ classesobject.py
@@ -0,0 +1,82 @@
+# class  Myclass:
+#     x = 5
+
+
+# p1 = Myclass()
+
+# print(p1.x)
+
+
+
+
+# class person:
+#     def __init__(self , name,age):
+#         self.name = name
+#         self.age = age
+
+# p1 = person("tunnu", 34)
+
+# print(p1.name)
+# print(p1.age)
+
+
+
+
+# class person:
+#     def __init__(self , name, age):
+
+#         self.name = name
+#         self.age = age
+
+# p2 = person("john",24)
+
+# print(p2.name)
+# print(p2.age)
+
+
+
+
+# class person:
+#     def __init__(self, name, age):
+
+#         self.name = name
+#         self.age = age
+
+#     def __str__(self):
+#        return f"{self.name} ({self.age})"
+
+# p1 = person("john", 23)
+
+# print(p1)
+
+
+
+
+# class person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def myfunc(self):
+#        print("hello my name is " + self.name)                                                                                                                   "  + self.name)
+
+# p1 = person("tunnu", 23)
+# p1.myfunc()
+
+
+
+
+
+# class person:
+#     def __init__(mysillyobject , name, age):
+#         mysillyobject.name = name
+#         mysillyobject.age = age
+
+#     def myfunc(abc):
+#         print("hello my name is " + abc.name)
+        
+
+# p1 = person("john",34)
+# p1.myfunc()
+
+
