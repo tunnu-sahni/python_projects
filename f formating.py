67a6aded657c8f94f31b9cffa1e77f0aed6bec59
tunnu-sahni <tsahni313@gmail.com>
2025-09-13 21:36:24 +0530

    added f formating.py

---
+++ f formating.py
@@ -0,0 +1,96 @@
+# txt = f"the price is 34 dollars"
+# print(txt)
+
+
+# price = 56
+# txt = f"the price is {price} dollars"
+# print(txt)
+
+
+# name = "tunnu sahni"
+# price = 45
+# place = "raipur"
+
+# print(f"my name is:{name} , my price is:{price} , my place is: {place}")
+
+
+# price = 45
+# txt = f"the price is {price:.2f} dollars"
+
+# print(txt)
+
+
+# txt = f"the price is {76:.2f} dollars"
+
+# print(txt)
+
+
+# txt = f"the price is {65:} dollars"
+
+# print(txt)
+
+
+# txt = f"the price is {20*46} dollars"
+
+# print(txt)
+
+
+# price = 59
+# tax = 0.25
+# txt = f"the price is {price +(price *tax)} dollars"
+
+# print(txt)
+
+
+# price = 400
+# tax = 2
+# txt = f"the price is {price +(price *tax)} "
+
+# print(txt)
+
+
+
+# price = 35
+# txt = f"it is very {'expensive' if price>40 else 'cheap'}"
+
+# print(txt)
+
+
+# fruit = "apple"
+# txt = f"i love {fruit.upper()}"
+
+# print(txt)
+
+
+
+# name = "tunnu"
+
+# txt = f"i love my name is: {name.upper()}"
+
+# print(txt)
+
+
+# convert into feet meters
+
+
+# def  myconverter(x):
+#     return x * 0.3048
+
+# txt = f"the plane is flying at a {myconverter(30000)} meter altitude"
+
+# print(txt)
+
+
+
+# def myconverter(x):
+#     return x * 1.3455
+
+# txt = f"the bus is speed of {myconverter(2000)}"
+
+# print(txt)
+
+
+price = 59000
+txt = f"the price is {price: ,} dollars"
+
+print(txt)
