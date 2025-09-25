0a18e1a9570bc44d00c23124424f4d01ccda8b98
tunnu-sahni <tsahni313@gmail.com>
2025-09-25 21:57:33 +0530

    added areacircumfrenc.py

---
+++ areacircumfrenc.py
@@ -0,0 +1,37 @@
+# radius = int(input("enter the radius of circle:"))
+# radius = int(input("enter the area of circumference :"))
+
+# area = 3.14 * radius * radius
+
+# print("the area of circle is:",area) 
+# circumference = 2 * 3.14 * radius
+
+# print("the circumference of circle is:",circumference ) 
+
+
+
+
+# radius = int(input("enter the area of circle :"))
+# radius = int(input("enter the area of circumference :"))
+
+# area = 3.14 * radius * radius
+# print("the area of circle :", area)
+
+# circumference = 2 * 3.14 * radius
+
+# print("the area of circumference is :",circumference)
+
+
+
+# area of the circle
+
+
+radius = int(input("enter the area of circle :"))
+
+area = 3.14 *radius * radius 
+
+print("the area of circle is :",area)
+
+circumference = 2 * 3.14 *radius 
+
+print("the area of circumference :",circumference)
