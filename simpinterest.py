6526d8e1c3ab9f463e1c1f33c0d6536591e77e67
tunnu-sahni <tsahni313@gmail.com>
2025-09-25 22:12:32 +0530

    added simpinterest.py

---
+++ simpinterest.py
@@ -0,0 +1,7 @@
+p = float(input("enter the principal amount :"))
+R = float(input("enter the rate of interest :"))
+
+T = float(input("enter the time :"))
+SI = (p*R*T)/100
+
+print(f"simple interest ={SI:.2f}")
